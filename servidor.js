312b633b0a3046c058ae3e20d097fe48621f83e0
matheus <socialfastpromo@gmail.com>
2025-01-16 14:15:21 -0300

    INITIAL COMMIT

--- servidor.js
+++ servidor.js
@@ -29,6 +29,9 @@ app.get('/verificar', async (req, res) => {
         console.log('Verificando o link:', linkTikTok);
         await page.goto(linkTikTok, { waitUntil: 'domcontentloaded', timeout: 60000 });
 
+        // Obter o código HTML da página
+        const pageContent = await page.content();
+
         // Verificar a presença da classe específica
         const classExists = await page.evaluate(() => {
             return !!document.querySelector('.css-gcssxn-DivSideNavMask.e8agtid1');
@@ -40,6 +43,7 @@ app.get('/verificar', async (req, res) => {
                 linkTikTok,
                 message: 'Vídeo encontrado.',
                 exists: true,
+                html: pageContent, // Adiciona o código HTML da página
             });
         } else {
             console.log('Vídeo não encontrado ou elemento ausente.');
@@ -47,6 +51,7 @@ app.get('/verificar', async (req, res) => {
                 linkTikTok,
                 message: 'Vídeo não encontrado ou elemento ausente.',
                 exists: false,
+                html: pageContent, // Adiciona o código HTML da página
             });
         }
     } catch (error) {
